8d18e4e78be74874f209a8862137476f50373021
Toralf Wittner <tw@dtex.org>
2014-12-09 23:05:01 +0100

    Update `eval-direct` example.

--- examples/eval-direct.hs
+++ examples/eval-direct.hs
@@ -8,12 +8,14 @@
 --
 -- The file "eval-routing.hs" does the same but using wai-routing.
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Control.Monad
 import Data.Attoparsec.ByteString.Char8
 import Data.ByteString (ByteString)
-import Data.ByteString.From
+import Data.ByteString.Conversion
 import Data.String
 import Network.HTTP.Types
 import Network.Wai
@@ -37,15 +39,15 @@ main :: IO ()
 main = run 8080 $ logStdout start
 
 start :: Application
-start r = case pathInfo r of
+start r k = case pathInfo r of
     "eval":[] ->
         let x = join $ lookup "x" (queryString r)
             y = join $ lookup "y" (queryString r)
             f = join $ lookup "f" (queryString r)
-        in either (respond status400 . fromString)
-                  (respond status200 . fromString . show)
+        in either (k . respond status400 . fromString)
+                  (k . respond status200 . fromString . show)
                   (eval f x y)
-    _ -> respond status404 ""
+    _ -> k $ respond status404 ""
 
 eval :: Maybe ByteString -> Maybe ByteString -> Maybe ByteString -> Either String Int
 eval (Just f) (Just x) (Just y) = do
@@ -59,5 +61,5 @@ eval (Just f) (Just x) (Just y) = do
         Div -> return (x' `div` y')
 eval _ _ _ = Left "invalid arguments"
 
-respond :: Monad m => Status -> Lazy.ByteString -> m Response
-respond s = return . responseLBS s []
+respond :: Status -> Lazy.ByteString -> Response
+respond s = responseLBS s []
